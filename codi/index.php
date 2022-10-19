b483cce43ca3401b423ea8616be6ad14de4552c6
grup9asix2 <grup9asix2@protonmail.com>
2022-10-19 19:28:57 +0200

    Calculadora IP darrera versió

--- codi/index.php
+++ codi/index.php
@@ -1,5 +1,20 @@
-<?php
-    echo "Hola món 20220930<br>";
-    echo "Som el grup03";
-    echo "Aquesta és l'activitat Gp2F4A6.3"
-?>
+<!DOCTYPE html>
+<html lang="ca">
+	<head>
+		<meta charset="utf-8">
+		<title>
+			PÀGINA INICIAL DE L'APLICACIÓ CALCULADORA
+		</title>
+	</head>
+	<body>
+		<p><b>CALCUALDORA ARITMÈTICA</b></p>
+		<a href="suma.html"><br>a) Suma de 2 operands</a>
+		<a href="resta.html"><br>b) Resta de 2 operands</a>
+		<a href="multiplicacio.html"><br>c) Multiplicacio de 2 operands</a>
+		<a href="divisio.html"><br>d) Divisio de 2 operands</a>
+		<br></br>
+		<a> --------------------------------------------------</a>
+		<a href="ip.html"><br>a) Calcul de subxarxes</a>
+
+	</body>
+<html>
